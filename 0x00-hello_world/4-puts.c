eb581861c3905a34eb79e25dc1c3a81897abacbc
Smhmhnd <smhando14@gmail.com>
2022-09-16 06:34:06 -0700

    my first prog

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /*
- * main-Print
+ * main-Printing
  *
  * return (0)
  */
 
-int main (void)
+int main(void)
 {
-   puts("Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
