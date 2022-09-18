eabde2d740146f6100854adca7f718faa060c5b5
Smhmhnd <smhando14@gmail.com>
2022-09-18 02:07:37 -0700

    stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main-Print to the standard error
+ * return Always 1 (Success)
+ */
+int main(void)
+{
+fprintf("and that piece of art is useful" "-" "Dora Korpar, 2015-10-19 \n", stderr);
+return (1);
+}
