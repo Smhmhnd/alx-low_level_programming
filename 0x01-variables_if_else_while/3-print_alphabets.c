824fa19323acc355d45301b36a074ef77986d710
Smhmhnd <smhando14@gmail.com>
2022-09-21 04:35:46 -0700

    a-Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main-Printing alPhabets
+ * retuns 0 (success)
+ */
+int main(void)
+{
+	int i;
+	for(i ='a'; i <= 'z'; i++)
+		{
+			putchar(i);
+		}
+	for(i ='A'; i <= 'Z'; i++)
+		{
+			putchar(i);
+		}
+	putchar('\n');
+	return (0);
+}
