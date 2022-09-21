0a31eeee14e68724fefabe0546776a3f891b45f1
Smhmhnd <smhando14@gmail.com>
2022-09-21 05:15:28 -0700

    a-Z

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-	int i;
-	for(i ='a'; i <= 'z'; i++)
-		{
-			putchar(i);
-		}
-	for(i ='A'; i <= 'Z'; i++)
-		{
-			putchar(i);
-		}
-	putchar('\n');
-	return (0);
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+for (i = 'A'; i <= 'Z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
